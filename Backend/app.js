b3d84f9bbb6c94a1103a028a52ee8b607b17450a
[SaadMu7ammad] <[saadmu7ammad711711@gmail.com]>
2023-09-27 17:44:47 +0300

    setup multer for imgs

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,10 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv/config';
 import productRoute from './routes/productRoutes.js';
 import userRoute from './routes/userRoutes.js';
 import orderRoute from './routes/orderRoutes.js';
+import uploadRoute from './routes/uploadRoutes.js';
 import cookieParser from 'cookie-parser';
 const app = express();
 import connectDB from './config/db.js';
@@ -13,10 +15,12 @@ app.use(cookieParser());
 app.use('/api/products', productRoute);
 app.use('/api/users', userRoute);
 app.use('/api/orders', orderRoute);
-app.get('/api', (req, res, next) => {
-  res.send('home');
-});
-app.get('/api/config/paypal',(req,res)=>res.send({clientId:process.env.PAYPAL_CLIENT_ID}))
+app.use('/api/upload', uploadRoute)
+
+app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
+
+const __dirname = path.resolve()//set dirname to the current directory
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 app.use(notFound);
 app.use(errorHandler);
 await connectDB();
